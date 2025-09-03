c7bd81485afabe1eee5b6f7d839c6ff9324da327
Ibrahim Sultan <sultanibrahim01@gmail.com>
2025-09-03 13:35:00 -0600

    some test updates

--- test/WordlerTest.java
+++ test/WordlerTest.java
@@ -3,6 +3,7 @@ package test;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -18,6 +19,7 @@ public class WordlerTest {
         ArrayList<String> failList = new ArrayList<String>();
         long totalTurnsTaken = 0;
         ArrayList<String> words = WordReader.readWords("././wordleWords.txt");
+        int[] results = {0,0,0,0,0,0,0};
         for (String word : words){
             Wordler wordler = new Wordler(words);
             Adversary adversary = new Adversary(word);
@@ -30,11 +32,18 @@ public class WordlerTest {
             if (turnsTaken >6){
                 passed = false;
                 failList.add(word);
+                results[6] = results[6]+1;
+            }
+            else{
+                results[turnsTaken-1] = results[turnsTaken-1]+1;
             }
             totalTurnsTaken = totalTurnsTaken+turnsTaken;
         }
         assertTrue("Failed on the following words: " + failList,passed);
         double average = ((double)totalTurnsTaken)/((double)words.size());
-        System.out.println("average time taken " +average);
+        System.out.println("average turns taken " +average);
+        for (int i=0;i<6;i++){
+            System.out.println(results[i]);
+        }
     }
 }
